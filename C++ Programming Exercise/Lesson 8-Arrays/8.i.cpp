d9dd13b5b2ca52140c9d26ac5c782df796e97f09
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-06-17 22:39:55 +0300

    Update 8.i.cpp

--- C++ Programming Exercise/Lesson 8-Arrays/8.i.cpp
+++ C++ Programming Exercise/Lesson 8-Arrays/8.i.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
+/*
+Amend the code to create an array of characters on the stack,
+ which contains your full name (including room for a
+space between your first and last names).
+Now create a function which prints to screen your name
+using the array you just created.
+*/
+
+string name[4]={"a","b","d","o"};
+int main(){
+
+ for(int i=0; i< 5; i++){
+    cout<<name[i]<<" ";
+};
+return 0;
+}
 
